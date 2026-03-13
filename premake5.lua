1de2046fab435f9ca29846738b6b79d5a135f13e
m-musona <musonamutale@gmail.com>
2026-03-13 07:32:04 +0200

    Add Premake File For Stakela

---
+++ premake5.lua
@@ -0,0 +1,34 @@
+project "bc7enc"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "off"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir    ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "bc7enc.cpp",
+        "bc7decomp.cpp",
+        "rgbcx.cpp",
+        "bc6h_enc.cpp",
+        "ert.cpp",
+    }
+
+    includedirs { "." }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
